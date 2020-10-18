ba4c36e936c766696a86bd4290285455dd3a7dfb
hamedfathi <noeffect@outlook.com>
2020-10-18 23:00:42 +0330

    init

---
+++ RazorClassLibrary1/Areas/Library/Pages/Shared/_Login.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorClassLibrary1.Areas.Library.Pages.Shared
+{
+    public class _LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
