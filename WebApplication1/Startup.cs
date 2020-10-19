e049da1c7453e513f31191388ff4e24ced1141cd
hamedfathi <noeffect@outlook.com>
2020-10-19 11:32:11 +0330

    update

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -23,7 +23,9 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages();   //razor page routing
             services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +50,7 @@ namespace WebApplication1
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();   //razor page routing
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
